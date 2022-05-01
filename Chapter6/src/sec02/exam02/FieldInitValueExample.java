e12045504d7a48ee2be5bd3a16f28c2e75c4228c
never7777 <79982654+never7777@users.noreply.github.com>
2022-05-01 23:58:42 +0900

    Field

---
+++ Chapter6/src/sec02/exam02/FieldInitValueExample.java
@@ -0,0 +1,14 @@
+package sec02.exam02;
+
+public class FieldInitValueExample {
+	public static void main(String[] args) {
+		FieldInitValue fiv = new FieldInitValue();
+	
+		System.out.println("ByteField: " + fiv.byteField);
+		System.out.println("shortField: " + fiv.shortField);
+		System.out.println("intField: " + fiv.intField);
+		System.out.println("longField: " + fiv.longField);
+		System.out.println("booleanField: " + fiv.booleanField);
+		System.out.println("ByteField: " + fiv.byteField);
+	}
+}
